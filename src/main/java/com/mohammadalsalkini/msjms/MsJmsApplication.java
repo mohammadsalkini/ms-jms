3942e8b790deeb59d9075626a427e38a75cc9792
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-12 07:00:51 +0200

    initial commit

---
+++ src/main/java/com/mohammadalsalkini/msjms/MsJmsApplication.java
@@ -0,0 +1,13 @@
+package com.mohammadalsalkini.msjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohammadalsalkini/msjms/MsJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammadalsalkini.msjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
