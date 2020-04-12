b1b1104d9fd1333b07b13f125e8f7462f7c2ee1c
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-12 07:25:41 +0200

    add JmsCnfig/TaskConfig

--- src/main/java/com/mohammadalsalkini/msjms/config/JmsConfig.java
+++ src/main/java/com/mohammadalsalkini/msjms/config/JmsConfig.java
@@ -1,9 +1,24 @@
 package com.mohammadalsalkini.msjms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
 /**
  * @author Mohammad Alsalkini
  * @project ms-jms
  * @created 12.04.2020 - 07:19
  */
+@Configuration
 public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_Type");
+        return converter;
+    }
 }
--- src/main/java/com/mohammadalsalkini/msjms/config/TaskConfig.java
+++ src/main/java/com/mohammadalsalkini/msjms/config/TaskConfig.java
@@ -1,9 +1,24 @@
 package com.mohammadalsalkini.msjms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * @author Mohammad Alsalkini
  * @project ms-jms
  * @created 12.04.2020 - 07:14
  */
+@EnableScheduling
+@EnableAsync
+@Configuration
 public class TaskConfig {
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
 }
